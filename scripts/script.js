6d3ad73624d02bb2c8a575b541d886190007864f
Evan <epmartinez@ucsd.edu>
2021-05-16 21:31:29 -0700

    got part 1 working

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,27 @@
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
+const settingsButton = document.querySelector('[alt="settings"]');
+const mainButton = document.querySelector("h1");
+const entryPage = document.querySelector("entry-page");
+entryPage.index = -1;
+
+function getEntryPageState()
+{
+  return {
+    entry: entryPage.entry,
+    entryIndex: entryPage.index
+  };
+}
+
+history.replaceState(getEntryPageState(), null, "");
+
+window.onpopstate = function(event){
+  setState(event.state);
+};
+
+mainButton.onclick = () => {history.pushState((getEntryPageState()), null, "./"); setState()};
+settingsButton.onclick = () => {history.pushState((getEntryPageState()), null, "settings"); setState()};
 
 // Make sure you register your service worker here too
 
@@ -13,6 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
+        let index = document.querySelector('main').childNodes.length;
+        newPost.onclick = () => {
+          entryPage.index = index;
+          let entryState = {
+            entry: entry,
+            entryIndex: index
+          };
+          history.pushState(entryState, null, "entry" + String(index));
+          setState(entryState)};
       });
     });
 });
