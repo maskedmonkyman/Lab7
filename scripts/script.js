c8b4b13b9bcde7d1eb90a53350cb5766d8fee055
Evan <epmartinez@ucsd.edu>
2021-05-16 23:29:03 -0700

    updated service worker register to point to correct locations

--- scripts/script.js
+++ scripts/script.js
@@ -27,7 +27,7 @@ settingsButton.onclick = () => {history.pushState((getEntryPageState()), null, "
 // Make sure you register your service worker here too
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, err => {
       console.log('ServiceWorker registration failed: ', err);
