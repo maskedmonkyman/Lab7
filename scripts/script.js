0731e70d3215a560d6c760be66571a17bf2dab30
Edmund Leibert III <edmund.leibert@gmail.com>
2021-05-16 22:00:23 -0700

    Minor change

--- scripts/script.js
+++ scripts/script.js
@@ -25,6 +25,18 @@ mainButton.onclick = () => {history.pushState((getEntryPageState()), null, "./")
 settingsButton.onclick = () => {history.pushState((getEntryPageState()), null, "settings"); setState()};
 
 // Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
